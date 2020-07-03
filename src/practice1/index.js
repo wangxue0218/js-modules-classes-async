b6c28507989dd2852a532220e3a0c2dfcf8f449e
xuewang <xuewang@thoughtworks.com>
2020-07-03 23:30:59 +0800

    implement practice1, 2, 3

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,6 @@
+import { ADDRESS, PORT, PATH, REGION } from "./constant";
+import getURL from "./utils";
 
+const url = getURL(ADDRESS, PORT, PATH);
+
+export { url, REGION as region };
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,7 @@
-export const getPoetry
+const url = "https://v1.jinrishici.com/all.json";
+export const getPoetry = async () => {
+    const result = await fetch(url)
+        .then((response) => response.json())
+        .catch((err) => console.log(err));
+    return [result.origin, result.author, result.content];
+};
